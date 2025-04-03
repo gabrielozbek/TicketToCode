8329b797391dd6e12adc3268eba5dbe5e853ab26
Johannesdsq <Johannes_03@live.se>
2025-04-03 14:13:33 +0200

    uppdaterat databasen

--- TicketToCode.Core/Data/Database.cs
+++ TicketToCode.Core/Data/Database.cs
@@ -6,10 +6,12 @@ public interface IDatabase
 {
     List<Event> Events { get; set; }
     List<User> Users { get; set; }
+    List<Booking> Bookings { get; set; }
 }
 
 public class Database : IDatabase
 {
     public List<Event> Events { get; set; } = new List<Event>();
     public List<User> Users { get; set; } = new List<User>();
+    public List<Booking> Bookings { get; set; } = new List<Booking>(); 
 }
