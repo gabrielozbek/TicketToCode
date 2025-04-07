2c8bc1dee0b81b9d992cdadd0520383470c6e4f2
Johannesdsq <Johannes_03@live.se>
2025-04-07 14:10:33 +0200

    skapat en endpoint som returnerer alla event

---
+++ TicketToCode.Api/Endpoints/EventGetAll.cs
@@ -0,0 +1,18 @@
+﻿using TicketToCode.Core.Data;
+using TicketToCode.Core.Models;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Http;
+using TicketToCode.Api.Endpoints;
+
+namespace TicketToCode.Api.Endpoints;
+
+public class EventGetAll : IEndpoint
+{
+    public static void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/api/events", (IDatabase db) =>
+        {
+            return Results.Ok(db.Events);
+        });
+    }
+}
